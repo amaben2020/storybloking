d766bc79c7e4777b29d584c98512f9901dba93bf
algomachine007 <amaben202phantom@gmail.com>
2024-05-05 11:11:08 +0100

    update an deployment

--- src/components/landing-page/index.tsx
+++ src/components/landing-page/index.tsx
@@ -1,14 +1,14 @@
 import { StoryblokComponent, storyblokEditable } from '@storyblok/react/rsc';
 
-const LandingPage = ({ blok, restaurants }: any) => {
-  console.log(blok);
-  console.log(restaurants);
+const LandingPage = ({ blok }: any) => {
+  console.log('BLOK', blok);
+  // console.log('RESTAURANTS', restaurants);
 
   return (
     <main {...storyblokEditable(blok)}>
       {blok?.body?.map((nestedBlok: any) => (
         <StoryblokComponent
-          restaurants={restaurants}
+          // restaurants={restaurants}
           blok={nestedBlok}
           key={nestedBlok._uid}
         />
